d11a56c17220f4e9d22fb64532c30fb54937d6e0
Marcelo Fontana <MarceVeloz@users.noreply.github.com>
2025-06-10 19:57:38 -0300

    First - Menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* 2 formas de llamar a las rutas
+  En la primera forma se importa el modulo de rutas y luego se llama a la funcion que retorna las rutas
+  En la segunda forma se importa el modulo de rutas previo a eso hay que exportar el modulo de rutas desde el archivo de rutas
+  */
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes')
+      .then(m => m.reactiveRoutes)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes')
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive'
+  }
+];
---
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,13 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { pipe } from 'rxjs';
+
+@Component({
+  selector: 'app-switches-page',
+  imports: [JsonPipe],
+  templateUrl: './switches-page.component.html',
+  styles: ``
+})
+export class SwitchesPageComponent {
+
+}
